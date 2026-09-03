d28cc7005e27f3d2fe2f9cf084715d2bc5e7af09
Rodrigo Gonzalez <rodrigogm@correo.ugr.es>
2026-09-03 17:13:22 +0200

    feat: scale_observations now doesn't reach a limit when the observations are too high

--- pendulum/src/utils/tflite.cpp
+++ pendulum/src/utils/tflite.cpp
@@ -3,6 +3,7 @@
 void scale_observations(float *obs, const int n) {
 	for (int i = 0; i < n; i++) {
 		float scaled = (obs[i] - MODEL_INPUT_MEAN[i]) / (sqrtf(MODEL_INPUT_VAR[i]) + 1e-8f);
-		obs[i] = fminf(fmaxf(scaled, -5.0), 5.0);
+		// obs[i] = fminf(fmaxf(scaled, -5.0), 5.0); //Uncomment if used trained scales
+		obs[i] = scaled;
 	}
 }
--- pendulum/src/utils/tflite.h
+++ pendulum/src/utils/tflite.h
@@ -4,9 +4,12 @@
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
-#include "./src/models/double_pendulum/first_try.h"
+#include "./src/models/double_pendulum/swingup_v1.h"
+// #include "./src/models/double_pendulum/swingup_v2.h"
+// #include "./src/models/double_pendulum/balancing.h"
 
-constexpr int kTensorArenaSize = 16 * 1024; // 32 32
+// constexpr int kTensorArenaSize = 16 * 1024; // 32 32
+constexpr int kTensorArenaSize = 48 * 1024; // 128 128
 extern uint8_t tensor_arena[kTensorArenaSize];
 
 extern const tflite::Model *model;
